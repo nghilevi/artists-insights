4357f83a625cf89b6084ef734cbac1929b2de9da
Nghi Le <vinhnghi223@gmail.com>
2022-10-09 16:34:42 +0300

    createInsights returns insights as an array

--- getArtistInsights.js
+++ getArtistInsights.js
@@ -30,7 +30,7 @@ async function defineWeight({id, weight, daysAgo}){ // return a number
 }
 
 function createInsights({formatResult, newsFormat, limit}){
-    return formatResult
+    const {insights} = formatResult
         .slice(0) // create a copy of formatResult for iterating
         .reduce((acc, result, _id, arr) => {
             if(acc.insights.length >= acc.limit){
@@ -40,6 +40,7 @@ function createInsights({formatResult, newsFormat, limit}){
             }
             return acc
         }, {insights:[], limit})
+    return insights
 }
 
 async function getArtistInsights({ id, limit, weight, daysAgo, newsFormat }) {
@@ -54,7 +55,7 @@ async function getArtistInsights({ id, limit, weight, daysAgo, newsFormat }) {
         )
 
         const insightsLimit = Math.abs(limit + (10 - definedWeight) * 200) // TODO should we ensure definedWeight <= 10 ?
-        const {insights} = createInsights({formatResult, newsFormat, limit: insightsLimit})
+        const insights = createInsights({formatResult, newsFormat, limit: insightsLimit})
 
         return { insights, ...(Boolean(newsFormat) && {weight: definedWeight}) };
 
