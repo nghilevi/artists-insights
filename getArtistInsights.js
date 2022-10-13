82e87606b1454ad3249a61c91d52c565e910be40
Nghi Le <vinhnghi223@gmail.com>
2022-10-13 19:14:49 +0300

    assigned functions to variables

--- getArtistInsights.js
+++ getArtistInsights.js
@@ -1,18 +1,20 @@
+'use strict';
+
 const ARTIST_INSIGHTS_QUERIES = QUERIES.QUERY_GET_ARTIST_INFO.ARTIST_INSIGHTS;
 const buildGetInsightsCountQuery = ARTIST_INSIGHTS_QUERIES.GET_INSIGHTS_COUNT;
 const buildGetArtistInsightsQuery = ARTIST_INSIGHTS_QUERIES.GET_ARTIST_INSIGHTS;
 
-function getInsightsCountFromDb({ id, highWeight, mediumWeight, daysAgo }){
+const getInsightsCountFromDb = function getInsightsCountFromDb({ id, highWeight, mediumWeight, daysAgo }){
     const query = buildGetInsightsCountQuery(id, highWeight, mediumWeight, daysAgo);
     return snowflakeClientExecuteQuery(query);
 }
 
-function getArtistInsightsFromDb({ id, limit, weight, daysAgo }){
+const getArtistInsightsFromDb = function getArtistInsightsFromDb({ id, limit, weight, daysAgo }){
     const query = buildGetArtistInsightsQuery(id, limit * 10, weight, daysAgo);
     return snowflakeClientExecuteQuery(query);
 }
 
-async function evaluateWeight ({id, daysAgo}) {
+const evaluateWeight = async function evaluateWeight ({id, daysAgo}) {
     const weight = {high: 8, medium: 4, low: 1};
     const counts = await getInsightsCountFromDb({id, highWeight: weight.high, mediumWeight: weight.medium, daysAgo});
     const [isHigh, isMedium] = [counts[0]?.count > 0, counts[1]?.count > 0];
@@ -23,7 +25,7 @@ async function defineWeight({id, weight, daysAgo}){ // return a number
     return weight === weight && typeof weight === 'number' ? weight : await evaluateWeight({id, daysAgo});
 }
 
-function createInsights({formattedInsights, isNewsFormat, limit}){
+const createInsights = function createInsights({formattedInsights, isNewsFormat, limit}){
     const insights = [];
     let i = 0;
     while(insights.length < limit && i < formattedInsights.length){ // reason using while-loop: https://hackernoon.com/3-javascript-performance-mistakes-you-should-stop-doing-ebf84b9de951
@@ -34,7 +36,7 @@ function createInsights({formattedInsights, isNewsFormat, limit}){
     return insights;
 }
 
-async function getArtistInsights({ id, limit, weight, daysAgo, newsFormat }) { // assume that id, limit, weight, daysAgo, newsFormat r in correct type
+const getArtistInsights = async function getArtistInsights({ id, limit, weight, daysAgo, newsFormat }) { // assume that id, limit, weight, daysAgo, newsFormat r in correct type
     try{
         const definedWeight = await defineWeight({id, weight, daysAgo});
         const sfResult = await getArtistInsightsFromDb({id, limit, definedWeight, daysAgo });
